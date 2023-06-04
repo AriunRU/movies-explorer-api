cd9aec0ee798be475053a221d50bac806d48f754
Кирилл Головин <ariunmlg@gmail.com>
2023-06-04 16:42:14 +0500

    DiplomBack

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const { celebrate } = require('celebrate');
 const {
   createUser, currentUser, updateUser,
 } = require('../controllers/users');
-const { getUserByIdValidation, updateUserProfileValidation } = require('../middlewares/validation');
+const { getUserByIdValidation, updateUserProfileValidation } = require('../utils/validations/userJoi');
 
 userRouter.post('/users', createUser);
 userRouter.get('/users/me', celebrate(getUserByIdValidation), currentUser);
