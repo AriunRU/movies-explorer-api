dcb52da133a87996d0a599922adee9ee6cab9c7f
Кирилл Головин <ariunmlg@gmail.com>
2023-06-05 21:54:59 +0500

    DiplomBack

--- backend/utils/token.js
+++ backend/utils/token.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const { SECRET_KEY = '12fbdb6e647a00514634eb137b2ecc64fc057f2c0f5c2006f5845e8a7d8a849e' } = process.env;
+const { SECRET_KEY = '12fbdb6e647a00600634eb137b2ecc64fc057f2c0f5c2006f5845e8a7d8a849e' } = process.env;
 
 function generateToken(payload) {
   return jwt.sign(payload, SECRET_KEY, { expiresIn: '7d' });
