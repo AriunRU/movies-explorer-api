50204cf450ac398e7d6aca2bbbe4f23010b9a0e1
Кирилл Головин <ariunmlg@gmail.com>
2023-06-07 21:25:57 +0500

    DiplomBack

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,12 @@ const { ERR_MESSAGE_WRONG_PAGE } = require('../utils/constants');
 const usersRoutes = require('./users');
 const moviesRoutes = require('./movie');
 
-const { validateSignInData, validateSignUpData } = require('../utils/validation');
+const { validateLogin, validateRegister } = require('../utils/validation');
 const { createUser, login, logout } = require('../controllers/users');
 const { auth } = require('../middlewares/auth');
 
-router.post('/signin', validateSignInData, login);
-router.post('/signup', validateSignUpData, createUser);
+router.post('/signin', validateLogin, login);
+router.post('/signup', validateRegister, createUser);
 
 router.use('/users', auth, usersRoutes);
 router.use('/movies', auth, moviesRoutes);
